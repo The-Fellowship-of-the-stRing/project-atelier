0e0dfd66cfa97d0ec99f07c82a125da0a1113589
Christian Lee <christianlee4037@gmail.com>
2023-11-16 22:48:01 -0800

    Get related product ids - Pass ids to card component

--- client/src/components/related_outfits/Card.jsx
+++ client/src/components/related_outfits/Card.jsx
@@ -3,11 +3,9 @@ import {useState, useEffect} from 'react';
 import getItemDetails from '../../utils/getItemDetails.js';
 
 const Card = ( {itemId} ) => {
-  let data = getItemDetails(itemId);
-  console.log(data);
-
   return (
     <div className="c card-container">
+      {itemId}
     </div>
   )
 }
---
+++ client/src/utils/getRelatedItems.js
@@ -0,0 +1,19 @@
+import axios from 'axios';
+
+const getRelatedItems = async (itemId) => {
+  try {
+    const headers = {
+      headers: {
+        "Authorization" : process.env.GIT_TOKEN
+      }
+    };
+    const url = process.env.GIT_API_URL;
+    const response = await axios.get(`${url}/products/${itemId}/related`, headers);
+
+    return response.data
+  } catch (err) {
+    console.log(console.error(err))
+  }
+};
+
+export default getRelatedItems;
