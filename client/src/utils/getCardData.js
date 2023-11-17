8ea2cdbe4a9c9eca1d14817c5981cd4395cd9b5c
Christian Lee <christianlee4037@gmail.com>
2023-11-17 14:20:38 -0800

    Built getCardData utils function. - Still working on transforming data properly

--- client/src/utils/getCardData.js
+++ client/src/utils/getCardData.js
@@ -1,5 +1,4 @@
 import axios from 'axios';
-// import getItemDetails from './getItemDetails.jsx';
 
 const getCardData = async (itemId) => {
   try {
@@ -10,27 +9,25 @@ const getCardData = async (itemId) => {
     };
     const url = process.env.GIT_API_URL;
     let response = {}
-    console.log("id", itemId);
     const idResponse = await axios.get(`${url}/products/${itemId}`, headers);
-    console.log("1. Product Data", idResponse.data)
     response.category = idResponse.data.category;
     response.features = idResponse.data.features;
 
     const stylesResponse = await axios.get(`${url}/products/${itemId}/styles`, headers);
-    console.log('Styles Res Pre: ', stylesResponse);
-    for (let key in stylesResponse.data) {
-      if(stylesResponse.data[key].default) {
-        stylesResponse.data = stylesResponse.data[key];
+
+    /* FIND DEFAULT STYLE */
+    let defaultStyle;
+    for (let style of stylesResponse.data.results) {
+      if(style["default?"]) {
+        defaultStyle = style;
       }
     }
-    console.log('Styles Res Post: ', stylesResponse);
-    response.name = stylesResponse.data.name;
-    response.features = stylesResponse.data.features;
-    response.photos = stylesResponse.data.photos;
-    response.original_price = stylesResponse.data.original_price;
+    response.name = defaultStyle.name;
+    response.photos = defaultStyle.photos;
+    response.original_price = defaultStyle.original_price;
 
-    console.log('Final response: ', response.data);
-    return response.data;
+    console.log('Final response: ', response);
+    return response;
   } catch (err) {
     console.error(err)
   }
