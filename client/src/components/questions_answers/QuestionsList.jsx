76ce06c0cfdd2e8b843a2487f9d70bde92f64636
kssampson <kylesampsonmusic@gmail.com>
2023-11-17 14:34:45 -0800

    add API call in questionsAnswers container, passed questionData to QuestionsList component

--- client/src/components/questions_answers/QuestionsList.jsx
+++ client/src/components/questions_answers/QuestionsList.jsx
@@ -3,6 +3,9 @@ import '../../stylesheets/questions_answers/questionsAnswers.css'
 
 const QuestionsList = () => {
 
+  //maps through question/answer data
+  //create component that has sturcture that shows all I want displayed
+  //map data handed from parent, rending a that component
   return (
     <div className="k-questions-list">
       QuestionsList Component
