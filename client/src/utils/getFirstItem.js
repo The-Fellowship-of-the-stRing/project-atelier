7f19c8d1f2c8891f33f17ea500887eccdc1e1ce9
Christian Lee <christianlee4037@gmail.com>
2023-11-17 09:54:35 -0800

    Added notes re async await syntax in getFirstItem.js1

--- client/src/utils/getFirstItem.js
+++ client/src/utils/getFirstItem.js
@@ -1,17 +1,21 @@
 import axios from 'axios';
 
+// must specify as async -> allows usage of await key
 const getFirstItem = async () => {
-  try {
+  try { // try something, if you fail, error will be caught
     const headers = {
       headers: {
         "Authorization" : process.env.GIT_TOKEN
       }
     };
     const url = process.env.GIT_API_URL;
+    // await must complete before following lines are executed
+    // await keyword must be used for promises
     const response = await axios.get(`${url}/products`, headers)
+    // once this executes, result will be returned as a promise that has been RESOLVED
     return response.data[0]
   } catch (err) {
-    console.log(console.error(err))
+    console.error(err)
   }
 };
 
