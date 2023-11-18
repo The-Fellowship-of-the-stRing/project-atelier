ed68f3fecdae039821f5bbb17fda08eb7d587e33
LukeLarson2 <lucas.m.larson2@gmail.com>
2023-11-18 09:04:32 -0800

    added formatting for review body

--- client/src/components/ratings_review/ReviewTile.jsx
+++ client/src/components/ratings_review/ReviewTile.jsx
@@ -1,16 +1,30 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import Stars from './Stars.jsx'
 
 import '../../stylesheets/ratings_review/reviewTile.css'
 
 const ReviewTile = ({review}) => {
+  const [body, setBody] = useState(review.body)
   const date = new Date(review.date)
+
+  const formatContent = () => {
+    let formatBody = review.body
+    if (formatBody > 250) {
+      formatBody = review.body.slice(0, 250);
+      formatBody = formatBody + '...';
+    }
+    setBody(formatBody)
+  }
 
   const formattedDate = date.toLocaleDateString("en-US", {
     year: 'numeric',
     month: 'long',
     day: 'numeric'
 });
+
+useEffect(() => {
+  formatContent()
+},[])
 
   return (
     <div className="l-review-tile-single">
@@ -24,7 +38,7 @@ const ReviewTile = ({review}) => {
         {review.summary}
       </div>
       <div className="l-review-tile-body">
-        {review.body}
+        {body}
       </div>
       <div className="l-review-tile-helpfulness">
         Helpful? <span className="l-review-tile-yes">Yes</span> ({review.helpfulness})
