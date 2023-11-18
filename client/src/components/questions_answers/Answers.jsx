eb38361cbc3eed8857f18a2a0904cc30a7bb72a1
kssampson <kylesampsonmusic@gmail.com>
2023-11-17 22:02:06 -0800

    set state for QuestionsAnswers container, made useeffects and functions to get, sort, and handle question data. Started building out QuestionsList with this data, and created a new Answers component to work with additonl answer data.

---
+++ client/src/components/questions_answers/Answers.jsx
@@ -0,0 +1,14 @@
+import React, { useEffect, useState } from 'react';
+import '../../stylesheets/questions_answers/questionsAnswers.css'
+
+const Answers = ( { questionId } ) => {
+
+  console.log('getting quesiton id hopefullly....', questionId)
+
+
+  return (
+    <div>Answers info</div>
+  )
+}
+
+export default Answers
