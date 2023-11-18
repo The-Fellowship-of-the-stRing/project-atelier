c944a33126554f4d81c7c804b51ec742796ba31b
Christian Lee <christianlee4037@gmail.com>
2023-11-17 16:04:09 -0800

    Working on helper function to get data for individual cards Recommiting

--- client/src/utils/getFirstItem.js
+++ client/src/utils/getFirstItem.js
@@ -14,9 +14,9 @@ const getFirstItem = async () => {
     // await keyword must be used for promises
     const response = await axios.get(`${url}/products`, headers)
     // once this executes, result will be returned as a promise that has been RESOLVED
-    return response.data[0]
+    return response.data[0];
   } catch (err) {
-    console.error(err)
+    console.error(err);
   }
 };
 
