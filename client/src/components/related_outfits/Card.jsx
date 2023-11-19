4b38260133722bf32a9a40456e624da1654085b4
Christian Lee <christianlee4037@gmail.com>
2023-11-18 16:05:19 -0800

    Added filler image if no image for a product exists

--- client/src/components/related_outfits/Card.jsx
+++ client/src/components/related_outfits/Card.jsx
@@ -31,10 +31,11 @@ const Card = ( {itemId, className} ) => {
   }
   /* REFACTOR ABOVE LATER: */
   // let priceString = (cardData && cardData.hasOwnProperty('sale_price')) ? (<p className="c-card-price"><s className="c-card-price-sales">${cardData.sale_price}</s> ${cardData.original_price}</p>) : (<p className="c-card-price">${cardData.original_price}</p>);
+  // console.log(cardData);
 
   return cardData ? (
     <div className={className}>
-      <img className="c-card-img" src={cardData.photos ? cardData.photos[0].thumbnail_url : ""} />
+      <img className="c-card-img" src={cardData.hasOwnProperty("photos") && cardData.photos[0].thumbnail_url !== null? cardData.photos[0].thumbnail_url : "https://www.generationsforpeace.org/wp-content/uploads/2018/03/empty.jpg"} />
       <p className="c-card-cat">{cardData.category}</p>
       <p className="c-card-name">{cardData.name}</p>
       {priceString}
