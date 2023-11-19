5c9292bb9348fb2a97ba99dd4426566401e8caaa
LukeLarson2 <lucas.m.larson2@gmail.com>
2023-11-18 17:10:24 -0800

    completed factors component and is ready for theme styling

--- client/src/components/ratings_review/RatingFactors.jsx
+++ client/src/components/ratings_review/RatingFactors.jsx
@@ -7,6 +7,15 @@ import '../../stylesheets/ratings_review/ratingFactors.css'
 const RatingFactors = ({totals}) => {
   const [categories, setCategories] = useState([])
 
+  const options = {
+    "Size": ["Too small", "Perfect", "Too Large"],
+    "Width": ["Too narrow", "Perfect", "Too Wide"],
+    "Comfort": ["Poor", "Ok", "Perfect"],
+    "Quality": ["Poor", "Ok", "Perfect"],
+    "Length": ["Runs short", "Perfect", "Runs long"],
+    "Fit": ["Runs tight", "Perfect", "Runs long"]
+  }
+
   useEffect(() => {
     if (totals) {
       const obj = totals.characteristics
@@ -24,19 +33,17 @@ const RatingFactors = ({totals}) => {
         const data = totals.characteristics[factor]
         const catRating = data.value;
         const markerLocation = (catRating / 5) * 100
+        const subText = options[factor]
         return (
           <div key={data.id} className="l-rating-factors-single">
             <div className="l-rating-factors-cat">
             {factor}
             </div>
             <div className="l-rating-back-container">
-              <div className="l-rating-back-bar" />
-              <div className="l-rating-back-bar" />
-              <div className="l-rating-back-bar" />
+              <div className="l-rating-back-bar"><span className="l-rating-sub-left">{subText[0]}</span></div>
+              <div className="l-rating-back-bar"><span className="l-rating-sub-mid">{subText[1]}</span></div>
+              <div className="l-rating-back-bar"><span className="l-rating-sub-right">{subText[2]}</span></div>
             <GoTriangleDown className="l-rating-marker" style={{left: `${markerLocation}%`}}/>
-            </div>
-            <div className="l-rating-sub-text">
-              <span>test</span><span>test</span><span>test</span>
             </div>
           </div>
         )
