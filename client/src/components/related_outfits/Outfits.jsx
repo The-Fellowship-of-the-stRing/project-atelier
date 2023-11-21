c2b7b572274f48d6a9e411017a73eeb9f1ae3662
Christian Lee <christianlee4037@gmail.com>
2023-11-20 20:18:42 -0800

    Refactor rendering logic for outfits

--- client/src/components/related_outfits/Outfits.jsx
+++ client/src/components/related_outfits/Outfits.jsx
@@ -4,21 +4,16 @@ import getRelatedItems from '../../utils/getRelatedItems.js';
 
 const Outfits = ( {itemId} ) => {
   const [outfitsByUser, setOutfitsByUser] = useState(null);
-  const [isAdded, setIsAdded] = useState(false);
+  const [isAdded, setIsAdded] = useState(null);
   // localStorage.clear();
 
   let parsedData = JSON.parse(localStorage.getItem(document.cookie));
   console.log("1. ", outfitsByUser);
-  console.log("State ", parsedData);
+  console.log("State ", outfitsByUser, isAdded);
 
   useEffect(() => {
-    if(parsedData) {
-      setOutfitsByUser(parsedData);
-    } else {
-      localStorage.setItem(document.cookie, '[]');
-      setOutfitsByUser([]);
-    }
-    // parsedData ? setOutfitsByUser(parsedData) : localStorage.setItem(document.cookie, '[]');
+    parsedData ? setOutfitsByUser(parsedData) : (localStorage.setItem(document.cookie, '[]') && setOutfitsByUser([]));
+    parsedData && parsedData.includes(itemId) ? setIsAdded(parsedData.includes(itemId)) : setIsAdded(false) ;
   }, []);
 
 
@@ -30,43 +25,30 @@ const Outfits = ( {itemId} ) => {
     setIsAdded(true);
   }
 
-  // const deleteProduct = () => {
-    //   console.log(`Add product id: ${itemId} to outfits with user id: ${userId}`)
-    //   setIsAdded();
-    // }
+  const deleteProduct = () => {
+      setIsAdded();
+    }
 
-      const nextClickHandler = () => {
-        console.log('Next clicked');
-      };
+  const nextClickHandler = () => {
+    console.log('Next clicked');
+  };
 
-  return (
+  return !isAdded ? (
     <div className="c-outfits-container">
       <h3>My Outfit</h3>
       <div className="c-card-container" >
       <button className="c-card-action-add" onClick={() => addProduct()}>+</button>
+      </div>
+      {outfitsByUser ? outfitsByUser.map((id,index) => (<Card className={`c-card-container c-card-${index}`} itemId={id} key={id} action="outfits"/>)) : null}
+      <button onClick={nextClickHandler}>></button>
     </div>
+  ) : (
+    <div className="c-outfits-container">
+      <h3>My Outfit</h3>
       {outfitsByUser ? outfitsByUser.map((id,index) => (<Card className={`c-card-container c-card-${index}`} itemId={id} key={id} action="outfits"/>)) : null}
       <button onClick={nextClickHandler}>></button>
     </div>
   )
-
-  // return !isAdded ? (
-  //   <div className="c-outfits-container">
-  //     <h3>My Outfit</h3>
-  //     <div className="c-card-container" >
-  //     <button className="c-card-action-add" onClick={() => addProduct()}>+</button>
-  //   </div>
-  //     {outfitsByUser ? outfitsByUser.map((id,index) => (<Card className={`c-card-container c-card-${index}`} itemId={id} key={id} action="outfits"/>)) : null}
-  //     <button onClick={nextClickHandler}>></button>
-  //   </div>
-  // ) : (
-  //   <div className="c-outfits-container">
-  //     <h3>My Outfit</h3>
-  //     {outfitsByUser ? outfitsByUser.map((id,index) => (<Card className={`c-card-container c-card-${index}`} itemId={id} key={id} action="outfits"/>)) : null}
-  //     <button onClick={nextClickHandler}>></button>
-  //   </div>
-  // )
-
 }
 /* How to persist data on client side */
 // local storage keyword to
