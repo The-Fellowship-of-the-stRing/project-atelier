8626731f0ac06dfce97ba06c42fbce947a668725
Gordon <gpchiu1213@gmail.com>
2023-11-24 04:16:25 -0800

    Progress Added

--- client/src/components/product_details/SocialMedia.jsx
+++ client/src/components/product_details/SocialMedia.jsx
@@ -1,8 +1,24 @@
+import React,{useState,useEffect} from 'react';
 const SocialMedia = () => {
+  // const [URL, setURL] = useState("localhost:8080")
   return(
-    <div className="g-socials">
-      SocialMedia
+    <div>
+    {/* // <div className="g-socials">
+    //  <a href='https://www.facebook.com/sharer/sharer.php?u=http://${}` 'target="_blank">
+    // <button>Click Me!</button>
+    // </a>
+    // <a href="destination.html" target="_blank">
+    //     <button>Click Me!</button>
+    // </a>
+    // <a href="destination.html" target="_blank">
+    //     <button>Click Me!</button>
+    // </a> */}
     </div>
   )
 }
 export default SocialMedia
+
+// https://www.facebook.com/sharer/sharer.php?u=http://localhost:8080
+// https://twitter.com/intent/tweet?url=http://localhost:3000&text=
+// https://pinterest.com/pin/create/button/?url=http://localhost:3000&media=&description=
+
