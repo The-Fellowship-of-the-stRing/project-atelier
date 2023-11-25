c856f49413d0fb82baaf12b6d25bd9044bae5a2f
kssampson <kylesampsonmusic@gmail.com>
2023-11-25 12:19:55 -0800

    placeholders, validity checking, and related state and functions for Add A Question

--- client/src/components/questions_answers/AddQuestion.jsx
+++ client/src/components/questions_answers/AddQuestion.jsx
@@ -6,22 +6,43 @@ import '../../stylesheets/questions_answers/AddQuestionModal.css'
 const AddQuestion = ( { itemId, itemName, handleQuestionModal, fetchQuestionData } ) => {
 
   const [yourQuestion, setYourQuestion] = useState('');
-  const [yourNickName, setYourNickname] = useState('');
+  const [yourNickName, setYourNickName] = useState('');
   const [yourEmail, setYourEmail] = useState('');
+  const [isInvalid, setIsInvalid] = useState(false);
 
-  const sendQuestionData = (e, closeModalFunc) => {
+  const checkValidity = () => {
+    if ((yourQuestion.length > 1000 || yourQuestion.length === 0) ||
+        (yourNickName.length > 60 || yourNickName.length === 0) ||
+        (yourEmail.length > 60 || yourEmail.length === 0) ||
+        !yourEmail.includes('@')) {
+      setIsInvalid(true)
+      setYourQuestion('');
+      setYourNickName('');
+      setYourEmail('');
+    } else {
+      setIsInvalid(false);
+    }
+  }
+
+  const sendQuestionData = (e) => {
     e.preventDefault();
-    axios.post(`/qa/questions`, {
-      product_id: itemId,
-      body: yourQuestion,
-      name: yourNickName,
-      email: yourEmail,
-    })
-    .then((response) => {
-      fetchQuestionData()
-      closeModalFunc()
-    })
-    .catch((err) => console.error(err))
+    checkValidity();
+    if (!isInvalid) {
+      axios.post(`/qa/questions`, {
+        product_id: itemId,
+        body: yourQuestion,
+        name: yourNickName,
+        email: yourEmail,
+      })
+      .then((response) => {
+        fetchQuestionData()
+      })
+      .then(() => {
+        alert('thank you for submitting!')
+        handleQuestionModal(false)
+      })
+      .catch((err) => console.error(err))
+    }
   }
 
 
@@ -33,7 +54,7 @@ const AddQuestion = ( { itemId, itemName, handleQuestionModal, fetchQuestionData
             <h2>{itemName}</h2>
             <div>
               <label className="k-question-container">
-                Your Question:
+                *Your Question:
                 <input
                 placeholder="Your question here..."
                 className="k-your-question"
@@ -42,21 +63,19 @@ const AddQuestion = ( { itemId, itemName, handleQuestionModal, fetchQuestionData
                 >
                 </input>
               </label>
-
                 <label className="k-nickname-container">
-                  What is Your Nickname:
+                  *What is Your Nickname:
                   <input
                   className="k-your-nickname"
                   placeholder="Example: jackson11!"
                   value={yourNickName}
-                  onChange={e => setYourNickname(e.target.value)}
+                  onChange={e => setYourNickName(e.target.value)}
                   >
                   </input>
                   <span className="k-nickname-privacy">For privacy reasons, do not use your full name.</span>
                 </label>
-
               <label className="k-email-container">
-                Your Email:
+                *Your Email:
                 <input
                 placeholder="Example: jack@email.com"
                 className="k-your-email"
@@ -69,13 +88,17 @@ const AddQuestion = ( { itemId, itemName, handleQuestionModal, fetchQuestionData
               <button
               type="button"
               className="k-add-queston-submit"
-              onClick={e => sendQuestionData(e, handleQuestionModal(false))}
-
+              onClick={e => sendQuestionData(e)}
+              // onClick={e => sendQuestionData(e)}
               >
                 Submit Your Question
               </button>
+              {isInvalid &&
+                <div>***You made an invalid entry***</div>
+              }
             </div>
             <AiOutlineClose className="k-add-question-close" onClick={() => handleQuestionModal(false)}/>
+          <span className="k-denotes-mandatory">* denotes mandatory field</span>
           </div>
         </div>
 
