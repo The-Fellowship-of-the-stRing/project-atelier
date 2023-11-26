3a3532c34376ca094c3243e23c68179f671b0ffe
Christian Lee <christianlee4037@gmail.com>
2023-11-26 01:07:45 -0800

    Update util function so that main product is not included in it's own related product ids array

--- client/src/utils/getRelatedItems.js
+++ client/src/utils/getRelatedItems.js
@@ -3,7 +3,9 @@ import axios from 'axios';
 const getRelatedItems = async (itemId) => {
   try {
     const response = await axios.get(`/products/${itemId}/related`);
-    return response.data
+    /* Remove main product from related products array */
+    let filteredResponse = response.data.filter((id) => itemId !== id);
+    return filteredResponse;
   } catch (err) {
     console.error(err)
   }
