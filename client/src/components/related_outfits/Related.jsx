8ca4c207dd295ff2e9faffda50f4c064b3043a69
Christian Lee <christianlee4037@gmail.com>
2023-11-25 17:15:22 -0800

    Added state to track visible cards. Reverting scroll and using next button to view more cards

--- client/src/components/related_outfits/Related.jsx
+++ client/src/components/related_outfits/Related.jsx
@@ -4,30 +4,48 @@ import getRelatedItems from '../../utils/getRelatedItems.js';
 
 const Related = ( {itemId, itemFeatures, itemName, updateMainProduct} ) => {
   const [relatedIds, setRelatedIds] = useState(null);
-
+  const [allCards, setAllCards] = useState(null);
+  const [visibleCards, setVisibleCards] = useState(null);
+  const [indexOfFirstVisibleCard, setIndexOfFirstVisibleCard] = useState(0);
   useEffect(() => {
     const fetchRelatedIds = async () => {
       try {
         const fetchedIds = await getRelatedItems(itemId);
         setRelatedIds(fetchedIds);
+        let cards = fetchedIds.map((id,index) => {
+          return id ? (
+            <Card className={`c-card c-card-${index}`} itemId={id} cardKey={id+itemId} key={id+itemId} itemName={itemName} itemFeatures={itemFeatures} action="related" updateMainProduct={updateMainProduct}/>
+            ) : null;
+          })
+        setAllCards(cards);
+        /* HARD CODING VISIBLE CARD COUNT TO 4 */
+        /* WILL MAKE DYNAMICALLY ADJUST SIZE BASED ON WINDOW SIZE */
+        let visibleCardsCount = 4;
+        setVisibleCards(cards.slice(0,4));
       } catch (err) {
         console.error('Error getting item details: ', err);
       }
     }
     fetchRelatedIds();
-  }, [itemId]);
+  }, []);
 
   const nextClickHandler = () => {
-    console.log('Next button clicked');
+    setVisibleCards([...visibleCards.slice(1), allCards[indexOfFirstVisibleCard+4]]);
+    setIndexOfFirstVisibleCard(indexOfFirstVisibleCard + 1);
   };
+
+  console.log(visibleCards);
 
   return relatedIds ? (
     <div className="c-related-container">
       <div className="c-cards">
-        {relatedIds.map((id,index) => {
-          return id ? (<Card className={`c-card c-card-${index}`} itemId={id} cardKey={id+itemId} key={id+itemId} itemName={itemName} itemFeatures={itemFeatures} action="related" updateMainProduct={updateMainProduct}/>) : null;
-          }
-        )}
+        {/* {relatedIds.map((id,index) => {
+          return id ? (
+            <Card className={`c-card c-card-${index}`} itemId={id} cardKey={id+itemId} key={id+itemId} itemName={itemName} itemFeatures={itemFeatures} action="related" updateMainProduct={updateMainProduct}/>
+            ) : null;
+          })
+        } */}
+        {visibleCards}
       </div>
       <p className="c-next"onClick={nextClickHandler}>></p>
     </div>
