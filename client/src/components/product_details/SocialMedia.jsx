6a482bf429d8af9b2c8312635eadfcaac0a6767a
Gordon <gpchiu1213@gmail.com>
2023-11-26 17:42:32 -0800

    Saving current progress

--- client/src/components/product_details/SocialMedia.jsx
+++ client/src/components/product_details/SocialMedia.jsx
@@ -1,8 +1,21 @@
 import React from 'react';
 
-const SocialMedia = () => (
-  <div className="g-socials">
-    SocialMedia
-  </div>
-);
+const SocialMedia = () => {
+  const facebook = 'https://www.facebook.com/sharer/sharer.php?u=http://localhost:8080';
+  const twitter = 'https://twitter.com/intent/tweet?url=http://localhost:8080';
+  const pinterest = 'https://pinterest.com/pin/create/button/?url=http://localhost:8080';
+  return (
+
+    <div className="g-socials">
+      <a href={facebook}> Twitter </a>
+      <a href={twitter}> Twitter </a>
+      <a href={pinterest}> Twitter </a>
+    </div>
+
+  );
+};
 export default SocialMedia;
+
+// localhost:8080
+// https://twitter.com/intent/tweet?url=http://localhost:3000&text=
+// https://pinterest.com/pin/create/button/?url=http://localhost:3000&media=&description=
