9377045ce6cadd7e0f05ae0f5e80c30d49049c9a
LukeLarson2 <lucas.m.larson2@gmail.com>
2023-11-27 10:37:55 -0800

    additional performance increase

--- client/src/components/ratings_review/SortDropDown.jsx
+++ client/src/components/ratings_review/SortDropDown.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import '../../stylesheets/ratings_review/sortDropDown.css';
 
 const SortDropDown = ({ sort, handleSort }) => (
-  <select defaultValue={sort} onChange={handleSort} className="l-sort-drop-down-list">
+  <select aria-label="Sort" defaultValue={sort} onChange={handleSort} className="l-sort-drop-down-list">
     <option value="relevance">relevance</option>
     <option value="newest">newest</option>
     <option value="helpful">helpful</option>
