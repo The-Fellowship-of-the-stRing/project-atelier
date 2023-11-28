a033450bc58d72f0de776a148c627b6255d2743a
Gordon <gpchiu1213@gmail.com>
2023-11-28 11:24:01 -0800

    Made Modal Exist, Adjusted Z-indexes for other files

--- client/src/components/product_details/ExpandedView.jsx
+++ client/src/components/product_details/ExpandedView.jsx
@@ -1,8 +1,22 @@
-import React from 'react';
+import '../../stylesheets/product_details/expandedView.css';
+import React, { useEffect, useState } from 'react';
+import ImageSelectExpand from './ImageSelectExpand.jsx';
 
-const ExpandedView = () => (
-  <div className="g-viewer">
-    ExpandedView
+const ExpandedView = ({
+  style, handleIndex, handlePage, currentIndex, lowIndex, highIndex,handleModalFalse
+}) => (
+  <div className="g-expanded-overlay">
+    <div className="g-expanded-modal">
+      <ImageSelectExpand
+        style={style}
+        handleIndex={handleIndex}
+        currentIndex={currentIndex}
+        handlePage={handlePage}
+        lowIndex={lowIndex}
+        highIndex={highIndex}
+      />
+      <button onClick={handleModalFalse} type="button"> Exit Expanded View </button>
+    </div>
   </div>
 );
 export default ExpandedView;
