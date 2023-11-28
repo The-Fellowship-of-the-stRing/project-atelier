a1ec31248cc7cf341daa595854c1f6ced48ffd0e
Gordon <gpchiu1213@gmail.com>
2023-11-28 12:11:37 -0800

    Saving progress to check main

--- client/src/components/product_details/ImageExpand.jsx
+++ client/src/components/product_details/ImageExpand.jsx
@@ -4,8 +4,24 @@ const ImageExpand = ({
   thumbnail, handleIndex, index, currentIndex, lowIndex, highIndex,
 }) => ((lowIndex <= index && highIndex > index) ? (
   <div>
-    {currentIndex !== index && <div onKeyDown={() => handleIndex(index)} onClick={() => handleIndex(index)} role="button" tabIndex={index}><img className="g-image_select-expand" src={thumbnail} alt="unselected item thumbnail" /></div>}
-    {currentIndex === index && <div onKeyDown={() => handleIndex(index)} onClick={() => handleIndex(index)} role="button" tabIndex={index}><img className="g-image_selected-expand" src={thumbnail} alt="unselected item thumbnail" /></div>}
+    {currentIndex !== index && (
+    <div onKeyDown={() => handleIndex(index)} onClick={() => handleIndex(index)} role="button" tabIndex={index}>
+      <img
+        className="g-image_select-expand"
+        src={thumbnail}
+        alt="unselected item thumbnail"
+      />
+    </div>
+    )}
+    {currentIndex === index && (
+    <div onKeyDown={() => handleIndex(index)} onClick={() => handleIndex(index)} role="button" tabIndex={index}>
+      <img
+        className="g-image_selected-expand"
+        src={thumbnail}
+        alt="unselected item thumbnail"
+      />
+    </div>
+    )}
   </div>
 )
   : <div />);
