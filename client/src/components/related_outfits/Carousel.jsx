7e2192aead04120f29ed3f7d9a743d85ea9ba686
Christian Lee <christianlee4037@gmail.com>
2023-11-30 11:28:06 -0800

    Some CSS

--- client/src/components/related_outfits/Carousel.jsx
+++ client/src/components/related_outfits/Carousel.jsx
@@ -18,34 +18,79 @@ const Carousel = ({
   // Each card is 200px wide with right-margin of 10px
   // const getCardCount = (w) => Math.floor((w - 40) / 210);
 
+  // useEffect(() => {
+  //   setVisibleCardCount(maxCardCount);
+  //   if (itemId !== mainId) {
+  //     setMainId(itemId);
+  //     setIndexOfFirstVisibleCard(0);
+  //     setVisibleCards(ids.slice(0, maxCardCount));
+  //     setIsPrevShown(false);
+  //     if (maxCardCount >= ids.length) {
+  //       setIsNextShown(false);
+  //     } else {
+  //       setIsNextShown(true);
+  //     }
+  //   } else {
+  //     let firstIndex = indexOfFirstVisibleCard;
+  //     let lastIndex = firstIndex + maxCardCount;
+  //     let newVisibleCards;
+  //     if (lastIndex === ids.length) {
+  //       newVisibleCards = ids.slice(firstIndex, lastIndex);
+  //     } else if (lastIndex < ids.length) {
+  //       newVisibleCards = ids.slice(firstIndex, lastIndex);
+  //     } else {
+  //       firstIndex = Math.max(0, ids.length - maxCardCount);
+  //       lastIndex = ids.length;
+  //       newVisibleCards = ids.slice(firstIndex, lastIndex);
+  //     }
+  //     setIndexOfFirstVisibleCard(firstIndex);
+  //     setVisibleCards(newVisibleCards);
+
+  //     if (lastIndex === ids.length) {
+  //       setIsNextShown(false);
+  //     } else {
+  //       setIsNextShown(true);
+  //     }
+  //     if (firstIndex === 0) {
+  //       setIsPrevShown(false);
+  //     } else {
+  //       setIsPrevShown(true);
+  //     }
+  //   }
+  // }, [maxCardCount, isAdded, ids]);
+
+  /* ---------------------- REFACTOR ---------------------- */
   useEffect(() => {
-    setVisibleCardCount(maxCardCount);
-    if (itemId !== mainId) {
-      setMainId(itemId);
-      setIndexOfFirstVisibleCard(0);
-      setVisibleCards(ids.slice(0, maxCardCount));
-      setIsPrevShown(false);
-      if (maxCardCount >= ids.length) {
-        setIsNextShown(false);
-      } else {
-        setIsNextShown(true);
-      }
+    let cardCount = maxCardCount;
+    if (!isAdded && action === 'outfits') {
+      cardCount -= 1;
+    }
+    setVisibleCardCount(cardCount);
+    setIndexOfFirstVisibleCard(0);
+    setMainId(itemId);
+    setVisibleCards(ids.slice(0, maxCardCount));
+    setIsPrevShown(false);
+    if (maxCardCount >= ids.length) {
+      setIsNextShown(false);
     } else {
+      setIsNextShown(true);
+    }
+  }, [ids]);
+
+  useEffect(() => {
+    if (itemId === mainId) {
       let firstIndex = indexOfFirstVisibleCard;
       let lastIndex = firstIndex + maxCardCount;
-      let newVisibleCards;
       if (lastIndex === ids.length) {
-        newVisibleCards = ids.slice(firstIndex, lastIndex);
+        setVisibleCards(ids.slice(firstIndex, lastIndex));
       } else if (lastIndex < ids.length) {
-        newVisibleCards = ids.slice(firstIndex, lastIndex);
+        setVisibleCards(ids.slice(firstIndex, lastIndex));
       } else {
         firstIndex = Math.max(0, ids.length - maxCardCount);
         lastIndex = ids.length;
-        newVisibleCards = ids.slice(firstIndex, lastIndex);
+        setVisibleCards(ids.slice(firstIndex, lastIndex));
       }
       setIndexOfFirstVisibleCard(firstIndex);
-      setVisibleCards(newVisibleCards);
-
       if (lastIndex === ids.length) {
         setIsNextShown(false);
       } else {
@@ -57,7 +102,50 @@ const Carousel = ({
         setIsPrevShown(true);
       }
     }
-  }, [maxCardCount, isAdded, ids]);
+  }, [maxCardCount]);
+  /*
+  useEffect(() => {
+    if (isAdded !== null && action === "outfits") {
+      let updatedCardCount;
+      let firstIndex;
+
+      if (isAdded) {
+        updatedCardCount = visibleCardCount - 1;
+        if (indexOfFirstVisibleCard + maxCardCount === ids.length) {
+          firstIndex = indexOfFirstVisibleCard + 1;
+          // setVisibleCardCount(updatedCardCount);
+          // setIndexOfFirstVisibleCard(firstIndex);
+          // setVisibleCards(ids.slice(firstIndex, firstIndex + updatedCardCount));
+        }
+        if (indexOfFirstVisibleCard + maxCardCount < ids.length) {
+          firstIndex = indexOfFirstVisibleCard;
+          // setVisibleCardCount(updatedCardCount);
+          // setIndexOfFirstVisibleCard(firstIndex);
+          // setVisibleCards(ids.slice(firstIndex, firstIndex + updatedCardCount));
+        }
+      } else { // !isAdded
+      // [1,2,3,4] curr [A,1,2] maxCount = 3 isAdd = true
+      // -> isAdd = false;
+      // [A,1,2]
+        updatedCardCount = Math.max(visibleCardCount + 1, ids.length);
+        if (indexOfFirstVisibleCard === 0) {
+          firstIndex = indexOfFirstVisibleCard;
+        }
+        if (indexOfFirstVisibleCard + maxCardCount <= ids.length) {
+          firstIndex = indexOfFirstVisibleCard - 1;
+        }
+        // if (indexOfFirstVisibleCard + maxCardCount < ids.length) {
+        //   firstIndex = indexOfFirstVisibleCard;
+        // }
+      }
+
+      setVisibleCardCount(updatedCardCount);
+      setIndexOfFirstVisibleCard(firstIndex);
+      setVisibleCards(ids.slice(firstIndex, firstIndex + updatedCardCount));
+    }
+  }, [isAdded]); */
+
+  /* ^^^^^^^^^^^^^^^^^^^^^^ REFACTOR ^^^^^^^^^^^^^^^^^^^^^^ */
 
   const updateVisibleCards = (incrementer) => {
     const updatedIndex = indexOfFirstVisibleCard + incrementer;
