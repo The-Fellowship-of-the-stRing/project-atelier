64ddf3040d5adce311284a043dff1349f36b306e
Christian Lee <christianlee4037@gmail.com>
2023-12-01 11:29:44 -0800

    Added click handlers for preview images

--- client/src/components/related_outfits/Card.jsx
+++ client/src/components/related_outfits/Card.jsx
@@ -13,12 +13,14 @@ const Card = ({
   updateMainProduct,
   isVisible,
 }) => {
+  const [cardImage, setCardImage] = useState(null);
   const [cardData, setCardData] = useState(null);
   const [isCardShown, setIsCardShown] = useState(true);
   const [isCompareShown, setIsCompareShown] = useState(false);
   const getCardData = async () => {
     try {
       const response = await fetchCardData(itemId);
+      setCardImage(response.photos);
       setCardData(response);
     } catch (err) {
       console.error('Error getting item details: ', err);
@@ -32,6 +34,11 @@ const Card = ({
       setIsCardShown(isVisible);
     }
   }, [isVisible]);
+
+  const updateImage = (newImage) => {
+    setCardImage(newImage);
+  };
+
   let priceString;
   if (cardData && cardData.sale_price && cardData.original_price) {
     priceString = (
@@ -112,12 +119,22 @@ const Card = ({
             data-testid="card-img"
             // onKeyPress={() => updateMainProduct(itemId)}
             // onClick={() => updateMainProduct(itemId)}
-            style={{ backgroundImage: `url(${cardData.photos})` }}
+            style={{ backgroundImage: `url(${cardImage})` }}
           />
           {actionButtons[action]}
           <div className="c-card-pre-img-container">
             {cardData.prePhotos.map((photo, index) => index < 4
-              && (<img className="c-card-pre-img" alt={photo.thumbnail_url} src={photo.thumbnail_url} />
+              && (
+              <div
+                role="button"
+                className="c-card-pre-img"
+                key={photo.thumbnail_url}
+                onClick={() => updateImage(photo.thumbnail_url)}
+                onKeyPress={() => updateImage(photo.thumbnail_url)}
+                tabIndex="0"
+                data-testid="card-img"
+                style={{ backgroundImage: `url(${photo.thumbnail_url})` }}
+              />
               ))}
           </div>
         </div>
